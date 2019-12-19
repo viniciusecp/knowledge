beb35d0f0c63858584aef05a110456af00db8e27
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2019-12-19 11:02:20 -0300

    Implemented toTree method

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -14,6 +14,9 @@ module.exports = app => {
     .get(app.api.category.get)
     .post(app.api.category.save);
 
+  // Cuidado com ordem! Tem que vir antes de /categories/:id
+  app.route("/categories/tree").get(app.api.category.getTree);
+
   app
     .route("/categories/:id")
     .get(app.api.category.getById)
