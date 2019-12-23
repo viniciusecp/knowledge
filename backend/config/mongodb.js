e15b6e6cb06678a07825909fbf2d644f92836f9b
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2019-12-23 14:17:30 -0300

    Implemented stats schedule

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect("mongodb://localhost/knowledge_stats", {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
   })
   .catch(err => {
     const msg = "ERRO! Não foi possível conectar com o MongoDB!";
