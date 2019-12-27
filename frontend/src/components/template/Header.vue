24d4852eff96861b29319b8516dd3ca1cf8c5d7f
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2019-12-27 09:52:40 -0300

    Implemented user menu component

--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -4,15 +4,20 @@
       <i class="fa fa-lg" :class="icon"></i>
     </a>
     <h1 class="title">{{ title }}</h1>
+    <UserDropdown v-if="!hideUserDropdown" />
   </header>
 </template>
 
 <script>
+import UserDropdown from "./UserDropdown";
+
 export default {
   name: "Header",
+  components: { UserDropdown },
   props: {
     title: String,
-    hideToggle: Boolean
+    hideToggle: Boolean,
+    hideUserDropdown: Boolean
   },
   computed: {
     icon() {
@@ -65,6 +70,7 @@ header.header > a.toggle {
 }
 
 header.header > a.toggle:hover {
+  color: #fff;
   background-color: rgba(0, 0, 0, 0.2);
 }
 </style>
