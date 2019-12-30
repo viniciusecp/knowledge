88768fc76246cb5d7cf33f0cb65984b2f564b3e9
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2019-12-30 10:31:48 -0300

    Finalizing dashboard component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,10 @@ import router from "./config/router";
 
 Vue.config.productionTip = false;
 
+// TEMPORARIO
+require("axios").defaults.headers.common["Authorization"] =
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MiwibmFtZSI6IkhvbmRhIENpdmljIiwiZW1haWwiOiJjaXZpY0B2aW5pY2l1cy5jb20iLCJhZG1pbiI6ZmFsc2UsImlhdCI6MTU3NzcxMTk4OSwiZXhwIjoxNTc3OTcxMTg5fQ.vPd6nuV9oe84pFxfDFikHitgq4cyuIs6kTIpVgfmC6o";
+
 new Vue({
   store,
   router,
