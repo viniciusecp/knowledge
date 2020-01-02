a87220a3bc89c7144e089f2093330f867148ba32
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2020-01-02 10:18:36 -0300

    Implemented user registration - 3

--- frontend/src/components/admin/UserAdmin.vue
+++ frontend/src/components/admin/UserAdmin.vue
@@ -10,6 +10,7 @@
               type="text"
               v-model="user.name"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o Node do Usuário..."
             ></b-form-input>
           </b-form-group>
@@ -21,13 +22,20 @@
               type="text"
               v-model="user.email"
               required
+              :readonly="mode === 'remove'"
               placeholder="Informe o E-mail do Usuário..."
             ></b-form-input>
           </b-form-group>
         </b-col>
       </b-row>
-      <b-form-checkbox id="user-admin" v-model="user.admin" class="mt-2 mb-3">Administrador</b-form-checkbox>
-      <b-row>
+      <b-form-checkbox
+        id="user-admin"
+        v-show="mode === 'save'"
+        v-model="user.admin"
+        class="mt-2 mb-3"
+        >Administrador</b-form-checkbox
+      >
+      <b-row v-show="mode === 'save'">
         <b-col md="6" sm="12">
           <b-form-group label="Senha:" label-for="user-password">
             <b-form-input
@@ -40,7 +48,10 @@
           </b-form-group>
         </b-col>
         <b-col md="6" sm="12">
-          <b-form-group label="Confirmação de senha:" label-for="user-confirm-password">
+          <b-form-group
+            label="Confirmação de senha:"
+            label-for="user-confirm-password"
+          >
             <b-form-input
               id="user-confirm-password"
               type="password"
@@ -51,12 +62,29 @@
           </b-form-group>
         </b-col>
       </b-row>
-      <b-button variant="primary" v-if="mode === 'save'" @click="save">Salvar</b-button>
-      <b-button variant="danger" v-if="mode === 'remove'" @cilck="remove">Excluir</b-button>
-      <b-button class="ml-2" @click="reset">Cancelar</b-button>
+      <b-row>
+        <b-col xs="12">
+          <b-button variant="primary" v-if="mode === 'save'" @click="save"
+            >Salvar</b-button
+          >
+          <b-button variant="danger" v-if="mode === 'remove'" @click="remove"
+            >Excluir</b-button
+          >
+          <b-button class="ml-2" @click="reset">Cancelar</b-button>
+        </b-col>
+      </b-row>
     </b-form>
     <hr />
-    <b-table hover striped :items="users" :fields="fields"></b-table>
+    <b-table hover striped :items="users" :fields="fields">
+      <template slot="actions" slot-scope="data">
+        <b-button variant="warning" @click="loadUser(data.item)" class="mr-2">
+          <i class="fa fa-pencil"></i>
+        </b-button>
+        <b-button variant="danger" @click="loadUser(data.item, 'remove')">
+          <i class="fa fa-trash"></i>
+        </b-button>
+      </template>
+    </b-table>
   </div>
 </template>
 
@@ -107,6 +135,7 @@ export default {
         .catch(showError);
     },
     remove() {
+      console.log("cuuu", this.user);
       const id = this.user.id;
       axios
         .delete(`${baseApiUrl}/users/${id}`)
@@ -115,6 +144,10 @@ export default {
           this.reset();
         })
         .catch(showError);
+    },
+    loadUser(user, mode = "save") {
+      this.mode = mode;
+      this.user = { ...user };
     }
   },
   mounted() {
@@ -123,5 +156,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
