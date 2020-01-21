20a7a815699ab3825449c2339ee0d9c00803312e
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2020-01-21 14:59:03 -0300

    Implemented articles by category - 1

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,7 +11,7 @@ Vue.config.productionTip = false;
 
 // TEMPORARIO
 require("axios").defaults.headers.common["Authorization"] =
-  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlZpbmljaXVzIEZhdXN0aW5vIFNpbHZhIiwiZW1haWwiOiJ2aW5pY2l1c0B2aW5pY2l1cy5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTc4NjU5OTI2LCJleHAiOjE1Nzg5MTkxMjZ9.iQAiMlF1HuAzaoN5zQrjjeH_ZnK_QzqegK_F86dLf_Q";
+  "bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IlZpbmljaXVzIEZhdXN0aW5vIFNpbHZhIiwiZW1haWwiOiJ2aW5pY2l1c0B2aW5pY2l1cy5jb20iLCJhZG1pbiI6dHJ1ZSwiaWF0IjoxNTc5NjI4NjE5LCJleHAiOjE1Nzk4ODc4MTl9.Ra4SO-Tjp9l-KbDdgwWPJPgw7ImIIfp_Er6Pw_OptXQ";
 
 new Vue({
   store,
