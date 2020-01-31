5726f59bc7f3e6d2424b652ae12a6b133a8c1d2f
Vinicius Faustino Silva <viniciusfs.ecp@gmail.com>
2020-01-31 16:04:16 -0300

    Implemented tree menu - 2

--- frontend/src/components/template/Menu.vue
+++ frontend/src/components/template/Menu.vue
@@ -37,7 +37,16 @@ export default {
     getTreeData() {
       const url = `${baseApiUrl}/categories/tree`;
       return axios(url).then(res => res.data);
+    },
+    onNodeSelect(node) {
+      this.$router.push({
+        name: "articlesByCategory",
+        params: { id: node.id }
+      });
     }
+  },
+  mounted() {
+    this.$refs.tree.$on("node:selected", this.onNodeSelect);
   }
 };
 </script>
